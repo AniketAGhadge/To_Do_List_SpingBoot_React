178cbd6eb3ef608844ab129bb47570c9ed1394db
AniketAGhadge <92659517+AniketAGhadge@users.noreply.github.com>
2024-02-26 17:37:32 +0530

    Delete studentsystem directory

--- studentsystem/src/main/java/com/example/studentsystem/service/StudentService.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentsystem.service;
-
-import com.example.studentsystem.model.Student;
-
-public interface StudentService {
-    public Student saveStudent(Student student);
-}
